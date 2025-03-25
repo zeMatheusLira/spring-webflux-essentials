bd0bd15afd72b3bb7e0aa55fa8d715317278052b
Zé Matheus <josemw86@gmail.com>
2025-03-25 11:57:47 -0300

    initial commit

---
+++ src/main/java/com/devdojo/springwebfluxessentials/SpringWebfluxEssentialsApplication.java
@@ -0,0 +1,18 @@
+package com.devdojo.springwebfluxessentials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.blockhound.BlockHound;
+
+@SpringBootApplication
+public class SpringWebfluxEssentialsApplication {
+
+//    static{
+//        BlockHound.install();
+//    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebfluxEssentialsApplication.class, args);
+    }
+
+}
